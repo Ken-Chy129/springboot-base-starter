63603318a7dbefabacf90d1f57d2384ee0cab21d
Ken-Chy129 <1294967895@qq.com>
2024-03-18 19:04:46 +0800

    Initial Commit

---
+++ src/main/java/cn/ken/SpringbootBaseStarterApplication.java
@@ -0,0 +1,13 @@
+package cn.ken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaseStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBaseStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ken/SpringbootBaseStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaseStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
